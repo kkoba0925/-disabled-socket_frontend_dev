d0b0a36742881edf02cb514f095b2cf515a62417
kkoba <k.koba0925@gmail.com>
2022-08-10 17:48:18 +0900

    20220810編集

---
+++ src/pages/tasks.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-row>
+    <v-col cols="7">
+      <v-toolbar
+        color="green darken-1"
+        
+      ></v-toolbar>
+      <v-card>
+        <v-row>
+          <v-col cols="2">
+            <v-btn 
+              color="success" 
+              fab
+              outlined
+            ></v-btn>
+          </v-col>
+          <v-col cols="8">
+            task
+          </v-col>
+          <v-col cols="2">
+            <v-btn 
+              color="purple darken-1"
+              fab
+              outlined
+            ></v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+    <v-col cols="5">
+      submenu
+    </v-col>
+  </v-row>
+</template>
